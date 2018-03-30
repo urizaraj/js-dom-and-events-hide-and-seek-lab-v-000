e99bbfa86fc2684e2be952ceb208f056b7fbb8a0
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 14:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function deepestChild() {
-  return document.querySelector('#grand-node ')
-}
-
 function increaseRankBy(n) {
   let items = document.querySelectorAll('ul.ranked-list li')
   items.forEach(item => {
     item.innerHTML = parseInt(item.innerHTML) + n
   })
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node ')
 }
