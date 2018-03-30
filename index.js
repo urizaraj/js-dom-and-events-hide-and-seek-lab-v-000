a6a7b5063e43a79ed0ffb6dc6d81e6e77e4bf838
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 14:23:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let items = document.querySelectorAll('ul.ranked-list li')
+  items.forEach(item => {
+    item.innerHTML = parseInt(item.innerHTML) + n
+  })
+}
+
+
+
+function deepestChild() {
+  let element = document.querySelector('#grand-node')
+  while (true) {
+    let child = element.querySelector('div')
+
+    if (child) {
+      element = child
+    } else {
+      break
+    }
+  }
+  return element
+}
