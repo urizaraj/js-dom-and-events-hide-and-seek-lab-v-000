0302d94c8cc21ca472efc3b699e1a13269bdb85c
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 14:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function deepestChild() {
+  return document.querySelector('#grand-node ')
+}
+
+function increaseRankBy(n) {
+  let items = document.querySelectorAll('ul.ranked-list li')
+  items.forEach(item => {
+    item.value = parseInt(item.value) + 1
+  })
+}
